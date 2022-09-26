cd9f0cb43464bab417eee83a60ac570233db95e7
Shivam Dubey <dshivam227@gmail.com>
2022-09-26 22:55:22 +0530

    day 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Program from './components/Hero/Programs/Program';
+import Reasons from './components/Reasons/Reasons';
 
 
 function App() {
   return (
     <div className="App">
    <Hero/>
-
+   <Program/>
+<Reasons/>
     </div>
   );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
-import Header from './Header/Header'
-import './Hero.css'
+import React from 'react';
+import Header from './Header/Header';
+import './Hero.css';
+
+
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+
+import Calories from "../../assets/calories.png";
+
+
+
+
+
 
 
 const Hero = () => {
@@ -40,15 +52,15 @@ const Hero = () => {
                     <div>
                         <span>+140</span>
                         <span>expert coaches</span>
-                        </div>
-                        <div>
+                    </div>
+                    <div>
                         <span>+978</span>
                         <span>members joined</span>
-                        </div>
-                        <div>
+                    </div>
+                    <div>
                         <span>+50</span>
                         <span>fitness programs</span>
-                        </div>
+                    </div>
                 </div>
 
                 {/* hero buttton */}
@@ -58,7 +70,31 @@ const Hero = () => {
                 </div>
 
             </div>
-            <div className="right-h">Right Side</div>
+            <div className="right-h">
+                <button className='btn'>Join Now</button>
+                <div className="heart-rate">
+                    <img src={Heart} alt="" />
+                    <span>Heart Rate</span>
+                    <span>116 bpm</span>
+                </div>
+
+                {/* hero image */}
+                <img src={hero_image} alt="" className='hero-image' />
+                <img src={hero_image_back} alt="" className='hero-image-back' />
+
+                {/* calories  */}
+                <div className="calories">
+                    <img src={Calories} alt="" />
+                    <div>
+                        <span>Calories Burned</span>
+                        <span>220 kcal</span>
+                    </div>
+
+                </div>
+
+
+
+            </div>
         </div>
     )
 }
